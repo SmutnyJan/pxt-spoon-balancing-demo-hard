ef70a887b20c81a819ec8cf5590a97a15a8405d0
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-16 16:15:35 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    jeHraSpustena = !(jeHraSpustena)
+})
+let souradnice: number[] = []
+let jeHraSpustena = false
+balancing.setDifficulty(Difficulty.Normal)
+basic.forever(function () {
+    if (jeHraSpustena) {
+        balancing.tick()
+        souradnice = balancing.coordinates()
+    }
+    if (jeHraSpustena && (souradnice[0] == 0 || souradnice[0] == 4 || souradnice[1] == 0 || souradnice[1] == 4)) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # # # #
+            # . . . #
+            `)
+        music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+        jeHraSpustena = false
+    }
+})
